27ea518d07088a50109a61e7429d6e649e8f27f9
Liann16 <liann.gonzales@email.lcup.edu.ph>
2025-09-09 22:30:21 +0800

    Revert "Home"
    This reverts commit ac761cc248e205907fb7c5fc7413321a0bdfb5f6.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./components/Header";
 import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <Header />
       <Home />
     </div>
   );
