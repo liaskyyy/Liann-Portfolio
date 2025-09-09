411e8bd48ed1ffe2b327e8e6d3fd5c35e26b665f
Liann16 <liann.gonzales@email.lcup.edu.ph>
2025-09-09 23:37:24 +0800

    Update portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,7 @@ function Contact() {
   return (
     <section id="contact" style={{ padding: "50px", textAlign: "center" }}>
       <h2>Contact Me</h2>
-      <p>Email: liann@example.com</p>
-      <p>Instagram: @who.is.llily</p>
-      <p>LinkedIn: linkedin.com/in/liann-gonzales</p>
+      <p>Email: lianngonzales7@ggmail.com</p>
     </section>
   );
 }
